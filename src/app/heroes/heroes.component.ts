92dc87e7546f99d92345e6a52bd13ea29819c595
leixingxing <986773381@qq.com>
2018-04-13 18:09:58 +0800

    添加详情功能

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 /*你要从 Angular 核心库中导入 Component 符号，并为组件类加上 @Component 装饰器。*/
-import {Component, NgModule, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
+import { HEROES } from '../mock-heroes';
 /*@Component 是个装饰器函数，用于为该组件指定 Angular 所需的元数据。*/
 
 /*selector— 组件的选择器（CSS 元素选择器）可以在是用这个标签的父级css中使用该标签作为标签选择器来添加样式
@@ -13,13 +14,12 @@ styleUrls— 组件私有 CSS 样式表文件的位置。
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {/*始终要 export 这个组件类，以便在其它地方（比如 AppModule）导入它。*/
-  /*hero = '哇';往HeroesComponent（HeroesComponent是一个对象）中添加一个 hero 属性，用来表示一个名叫 “Windstorm” 的英雄。*/
-  hero: Hero = {
-    id: 1,
-    name: 'Superman',
-    skill: 'JS'
-  };
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(dada: Hero): void {
+    this.selectedHero = dada;
+  }
   constructor() { }
 
   ngOnInit() {/*ngOnInit 是一个生命周期钩子，Angular 在创建完组件后很快就会调用 ngOnInit。这里是放置初始化逻辑的好地方。*/
