d17f51e353eb775ebb6ff2660d3b023aee16ba06
leixingxing <986773381@qq.com>
2018-04-13 18:10:14 +0800

    添加详情功能

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
-
 @NgModule({
-  /*declarations声明组件;每个组件都必须声明在（且只能声明在）一个 NgModule 中。注意 AppModule 声明了应用中的所有组件，AppComponent 和 HeroesComponent。使用ng g component 组件名称   g指generate的简写表示生成*/
   declarations: [
     AppComponent,
     HeroesComponent
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from './hero';
+
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Mr.Nice', skill: 'JS'},
+  { id: 12, name: 'Narco' , skill: 'TS'},
+  { id: 13, name: 'Bombasto' , skill: 'HTML'},
+  { id: 14, name: 'Celeritas' , skill: 'JS'},
+  { id: 15, name: 'Magneta' , skill: 'JS'},
+  { id: 16, name: 'RubberMan' , skill: 'JS'},
+  { id: 17, name: 'Dynama' , skill: 'CSS'},
+  { id: 18, name: 'Dr IQ' , skill: 'JS'},
+  { id: 19, name: 'Magma' , skill: 'SASS'},
+  { id: 20, name: 'Tornado', skill: 'LESS' }
+];
