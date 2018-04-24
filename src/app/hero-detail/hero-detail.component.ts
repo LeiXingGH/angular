9d22e4da32fefb975a3fe2e3b09b46ad77853f0c
leixingxing <is_leixingxing@163.com>
2018-04-24 17:52:43 +0800

    服务

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -7,7 +7,7 @@ import { Hero } from '../hero';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  @Input() hero: Hero;//hero 属性必须是一个带有 @Input() 装饰器的输入属性，因为外部的 HeroesComponent 组件将会绑定到它。就像这样<app-hero-detail [hero]="selectedHero"></app-hero-detail>
   constructor() { }
 
   ngOnInit() {
